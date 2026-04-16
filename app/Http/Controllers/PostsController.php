7434e3baa3ed6affb927d70a6dd8069993979342
yoga <yoga@yogas-MacBook-Pro.local>
2026-04-16 07:59:29 +0700

    delete post api

--- app/Http/Controllers/PostsController.php
+++ app/Http/Controllers/PostsController.php
@@ -8,7 +8,7 @@ use Illuminate\Support\Facades\Validator; // import validator
 
 class PostsController extends Controller
 {
-    // mengambil semua data
+    // function index mengambil semua data posts
     public function index()
     {
         $posts = Post::get(); // mengambil semua data dari model Post
@@ -19,8 +19,7 @@ class PostsController extends Controller
         ]);
     }
 
-    // menyimpan data
-    // function store
+    // function store menyimpan data
     public function store(Request $request)
     {
         $validator = Validator::make($request->all(), [
@@ -49,5 +48,56 @@ class PostsController extends Controller
             'message' => 'Berhasil membuat post',
             'data' => $post
         ], 201);
+    }
+
+    // function show detail posts
+    public function show($id)
+    {
+        $post = Post::find($id);
+
+        return response()->json([
+            'success' => true,
+            'data' => $post
+        ], 200);
+    }
+
+    // function update
+    public function update($id, Request $request)
+    {
+        $validator = Validator::make($request->all(), [
+            'content' => 'required|string|max:255',
+            'image_url' => 'nullable'
+        ]);
+
+        // check validator jika gagal
+        if ($validator->fails()) {
+            return response()->json([
+                'success' => false,
+                'message' => $validator->error()
+            ], 400);
+        }
+
+        // jika validator sukses
+        $post = Post::find($id);
+        // Tampung data baru
+        $post->content = $request->content;
+        $post->image_url = $request->image_url;
+        $post->save();
+
+        return response()->json([
+            'success' => true,
+            'message' => 'Berhasil mengupdate data',
+            'data' => $post
+        ]);
+    }
+
+    // function destroy menghapus data berdasarkan id
+    public function destroy($id)
+    {
+        Post::destroy($id);
+        return response()->json([
+            'success' => true,
+            'message' => 'Post berhasil dihapus'
+        ]);
     }
 }
