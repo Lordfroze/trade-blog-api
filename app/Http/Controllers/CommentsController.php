deff63ef6c10c224bd24a5e671d67b1fed6557a7
yoga <yoga@yogas-MacBook-Pro.local>
2026-04-16 09:33:14 +0700

    add comments

---
+++ app/Http/Controllers/CommentsController.php
@@ -0,0 +1,41 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Validator; // import validator
+use App\Models\Comment;
+
+
+class CommentsController extends Controller
+{
+    // Simpan komentar baru
+    public function store(Request $request)
+    {
+        $validator = Validator::make($request->all(), [
+            'user_id' => 'required',
+            'post_id' => 'required',
+            'content' => 'required|string|max:255'
+        ]);
+
+        if ($validator->fails()) {
+            return response()->json([
+                'success' => false,
+                'message' => $validator->errors()
+            ]);
+        }
+
+        // Simpan komentar baru ke dalam database
+        $comment = Comment::create([
+            'user_id' => $request->user_id,
+            'post_id' => $request->post_id,
+            'content' => $request->content
+        ]);
+
+        return response()->json([
+            'success' => true,
+            'message' => 'Komentar berhasil ditambahkan',
+            'data' => $comment
+        ]);
+    }
+}
--- routes/api.php
+++ routes/api.php
@@ -3,17 +3,25 @@
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\PostsController;
+use App\Http\Controllers\CommentsController;
 
 
 Route::get('/user', function (Request $request) {
     return $request->user();
 })->middleware('auth:sanctum');
 
-// prefix untuk menentukan endpoint
+// prefix untuk menentukan endpoint posts
 Route::prefix('posts')->group(function () {
     Route::get('/', [PostsController::class, 'index']); // mengambil semua data
     Route::post('/', [PostsController::class, 'store']); // menyimpan data
     Route::get('{id}', [PostsController::class, 'show']); // mengambil data berdasarkan id
     Route::put('{id}', [PostsController::class, 'update']); // mengupdate data berdasarkan id
     Route::delete('{id}', [PostsController::class, 'destroy']); // menghapus data berdasarkan id
+});
+
+// prefix untuk menentukan endpoint comments
+// Menghandle Comment
+Route::prefix('comments')->group(function () {
+    Route::post('/', [CommentsController::class, 'store']); // Simpan komentar baru
+    Route::delete('{id}', [CommentsController::class, 'destroy']); // Menghapus komentar
 });
