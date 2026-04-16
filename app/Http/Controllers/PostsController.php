5b6b9d9280404bb0a8bc7a587a9f3d4da5b16947
yoga <yoga@yogas-MacBook-Pro.local>
2026-04-16 07:33:25 +0700

    add posts

--- app/Http/Controllers/PostsController.php
+++ app/Http/Controllers/PostsController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use App\Models\Post;
+use Illuminate\Support\Facades\Validator; // import validator
 
 class PostsController extends Controller
 {
@@ -16,5 +17,37 @@ class PostsController extends Controller
             'success' => true,
             'data' => $posts
         ]);
+    }
+
+    // menyimpan data
+    // function store
+    public function store(Request $request)
+    {
+        $validator = Validator::make($request->all(), [
+            'user_id' => 'required',
+            'content' => 'required|string|max:255',
+            'image_url' => 'nullable'
+        ]);
+
+        // check validator jika gagal
+        if ($validator->fails()) {
+            return response()->json([
+                'success' => false,
+                'message' => $validator->errors()
+            ], 400);
+        }
+
+        // jika validator sukses
+        $post = Post::create([
+            'user_id' => $request->user_id,
+            'content' => $request->content,
+            'image_url' => $request->image_url
+        ]);
+
+        return response()->json([
+            'success' => true,
+            'message' => 'Berhasil membuat post',
+            'data' => $post
+        ], 201);
     }
 }
